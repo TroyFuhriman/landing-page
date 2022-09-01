445c6a0fba46047eb93bdb409b9ebdf9288fa6eb
Troy Fuhriman <tkfuhriman@gmail.com>
2022-08-31 23:01:53 -0600

    init

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// This is how to send the email
+// const templateParams = {
+//   from_name: "James",
+//   to_name: "Troy Fuhriman",
+//   message: "Check this out!",
+//  reply_to:"troy.fuhriman@microbeformulas.com"
+// };
+// emailjs.send("service_xzps5ii", "template_nii6el7", templateParams);
